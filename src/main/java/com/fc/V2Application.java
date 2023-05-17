39cb33c8aefe64ad8e35db5ac4dfac2b882a5ae0
MoboGuo <GUOmb960113@163.com>
2023-05-17 15:14:15 +0800

    提交测试

--- src/main/java/com/fc/V2Application.java
+++ src/main/java/com/fc/V2Application.java
@@ -10,7 +10,7 @@ public class V2Application {
     public static void main(String[] args) {
 
         SpringApplication.run(V2Application.class, args);
-        System.out.println("*******************************************\n" +
+        System.out.println("********************************************\n" +
         		"* 码云地址                                  *\n" +
         		"* https://gitee.com/bdj/SpringBoot_v2     *\n" +
         		"*                                         *\n" +
