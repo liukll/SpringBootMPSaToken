20df54f23a3f463333c66069ab07fbfba58256d2
lkle <1446244720@qq.com>
2023-05-19 01:34:34 +0800

    静态页面及资源提交

--- src/main/java/com/fc/v2/controller/admin/StudyTogetherController.java
+++ src/main/java/com/fc/v2/controller/admin/StudyTogetherController.java
@@ -29,141 +29,149 @@ import java.util.List;
 
 /**
  * 学习记录表Controller
- * @ClassName: StudyTogetherController
+ *
  * @author fuce
+ * @ClassName: StudyTogetherController
  * @date 2023-05-17 15:31:03
  */
 @Api(value = "学习记录表")
 @Controller
 @RequestMapping("/StudyTogetherController")
-public class StudyTogetherController extends BaseController{
-	
-	private String prefix = "admin/studyTogether";
-	
-	@Autowired
-	private StudyTogetherService studyTogetherService;
-	
-	
-	/**
-	 * 学习记录表页面展示
-	 * @param model
-	 * @return String
-	 * @author fuce
-	 */
-	@ApiOperation(value = "分页跳转", notes = "分页跳转")
-	@GetMapping("/view")
-	@SaCheckPermission("gen:studyTogether:view")
-    public String view(ModelMap model)
-    {
+public class StudyTogetherController extends BaseController {
+
+    private String prefix = "admin/studyTogether";
+
+    @Autowired
+    private StudyTogetherService studyTogetherService;
+
+
+    /**
+     * 学习记录表页面展示
+     *
+     * @param model
+     * @return String
+     * @author fuce
+     */
+    @ApiOperation(value = "分页跳转", notes = "分页跳转")
+    @GetMapping("/view")
+    @SaCheckPermission("gen:studyTogether:view")
+    public String view(ModelMap model) {
         return prefix + "/list";
     }
-	
 
-	@ApiOperation(value = "分页跳转", notes = "分页跳转")
-	@GetMapping("/list")
-	@SaCheckPermission("gen:studyTogether:list")
-	@ResponseBody
-	public ResultTable list(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer limit , StudyTogether studyTogether){
-		PageHelper.startPage(page, limit);
-		TsysUser tsysUser = SaTokenUtil.getUser();
-		List<StudyTogether> studyTogetherList = studyTogetherService.lambdaQuery()
-				.eq(StudyTogether::getCreateUser, tsysUser.getUsername())
-				.list();
-		PageInfo<StudyTogether> pageInfo = new PageInfo<>(studyTogetherList);
-		return pageTable(pageInfo.getList(),pageInfo.getTotal());
-	}
-	
-	/**
+
+    @ApiOperation(value = "分页跳转", notes = "分页跳转")
+    @GetMapping("/list")
+    @SaCheckPermission("gen:studyTogether:list")
+    @ResponseBody
+    public ResultTable list(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer limit, StudyTogether studyTogether) {
+        PageHelper.startPage(page, limit);
+        TsysUser tsysUser = SaTokenUtil.getUser();
+        List<StudyTogether> studyTogetherList = studyTogetherService.lambdaQuery()
+                .eq(StudyTogether::getCreateUser, tsysUser.getUsername())
+                .list();
+        PageInfo<StudyTogether> pageInfo = new PageInfo<>(studyTogetherList);
+        return pageTable(pageInfo.getList(), pageInfo.getTotal());
+    }
+
+    /**
      * 新增跳转
      */
-	@ApiOperation(value = "新增跳转", notes = "新增跳转")
+    @ApiOperation(value = "新增跳转", notes = "新增跳转")
     @GetMapping("/add")
-    public String add(ModelMap modelMap)
-    {
+    public String add(ModelMap modelMap) {
         return prefix + "/add";
     }
-	
+
     /**
      * 新增保存
-     * @param 
+     *
+     * @param
      * @return
      */
-	@ApiOperation(value = "新增", notes = "新增")
-	@PostMapping("/add")
-	@SaCheckPermission("gen:studyTogether:add")
-	@ResponseBody
-	public AjaxResult add(StudyTogether studyTogether, HttpServletRequest request){
-		TsysUser tsysUser = SaTokenUtil.getUser();
-		studyTogether.setUserId(Long.valueOf(tsysUser.getId()));
-		studyTogether.setUserName(tsysUser.getUsername());
-		studyTogether.setCreateIp(request.getRemoteAddr());
-		studyTogether.setCreateTime(new Date());
-		studyTogether.setCreateUser(tsysUser.getUsername());
-		Long startTime = studyTogether.getStudyStartTime().getTime();
-		Long endTime = studyTogether.getStudyEndTime().getTime();
-		float time = new BigDecimal((endTime - startTime) / 1000 / 60)
-				.setScale(1, RoundingMode.HALF_UP).floatValue();
-		studyTogether.setTodayStudyCount(time);
-		boolean rtn = studyTogetherService.save(studyTogether);
-		if(rtn){
-			return success();
-		}else{
-			return error();
-		}
-	}
-	
-	/**
-	 * 学习记录表删除
-	 * @param ids
-	 * @return
-	 */
-	//@Log(title = "学习记录表删除", action = "111")
-	@ApiOperation(value = "删除", notes = "删除")
-	@DeleteMapping("/remove")
-	@SaCheckPermission("gen:studyTogether:remove")
-	@ResponseBody
-	public AjaxResult remove(String ids){
-		boolean rtn = studyTogetherService.removeBatchByIds(Arrays.asList(ids.split(",")));
-		if(rtn) {
-			return success();
-		} else {
-			return error();
-		}
-	}
-	
-	
-	/**
-	 * 修改跳转
-	 * @param id
-	 * @param mmap
-	 * @return
-	 */
-	@ApiOperation(value = "修改跳转", notes = "修改跳转")
-	@GetMapping("/edit/{id}")
-    public String edit(@PathVariable("id") String id, ModelMap map)
-    {
+    @ApiOperation(value = "新增", notes = "新增")
+    @PostMapping("/add")
+    @SaCheckPermission("gen:studyTogether:add")
+    @ResponseBody
+    public AjaxResult add(StudyTogether studyTogether, HttpServletRequest request) {
+        TsysUser tsysUser = SaTokenUtil.getUser();
+        studyTogether.setUserId(Long.valueOf(tsysUser.getId()));
+        studyTogether.setUserName(tsysUser.getUsername());
+        studyTogether.setCreateIp(request.getRemoteAddr());
+        studyTogether.setCreateTime(new Date());
+        studyTogether.setCreateUser(tsysUser.getUsername());
+        Long startTime = studyTogether.getStudyStartTime().getTime();
+        Long endTime = studyTogether.getStudyEndTime().getTime();
+        float time = new BigDecimal((endTime - startTime) / 1000 / 60)
+                .setScale(1, RoundingMode.HALF_UP).floatValue();
+        studyTogether.setTodayStudyCount(time);
+        boolean rtn = studyTogetherService.save(studyTogether);
+        if (rtn) {
+            return success();
+        } else {
+            return error();
+        }
+    }
+
+    /**
+     * 学习记录表删除
+     *
+     * @param ids
+     * @return
+     */
+    //@Log(title = "学习记录表删除", action = "111")
+    @ApiOperation(value = "删除", notes = "删除")
+    @DeleteMapping("/remove")
+    @SaCheckPermission("gen:studyTogether:remove")
+    @ResponseBody
+    public AjaxResult remove(String ids) {
+        boolean rtn = studyTogetherService.removeBatchByIds(Arrays.asList(ids.split(",")));
+        if (rtn) {
+            return success();
+        } else {
+            return error();
+        }
+    }
+
+
+    /**
+     * 修改跳转
+     *
+     * @param id
+     * @param mmap
+     * @return
+     */
+    @ApiOperation(value = "修改跳转", notes = "修改跳转")
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") String id, ModelMap map) {
         map.put("StudyTogether", studyTogetherService.getById(id));
 
         return prefix + "/edit";
     }
 
-	/**
+    /**
      * 修改保存
      */
     //@Log(title = "学习记录表修改", action = "111")
-	@ApiOperation(value = "修改保存", notes = "修改保存")
+    @ApiOperation(value = "修改保存", notes = "修改保存")
     @SaCheckPermission("gen:studyTogether:edit")
     @PostMapping("/edit")
     @ResponseBody
-    public AjaxResult editSave(StudyTogether studyTogether)
-    {
+    public AjaxResult editSave(StudyTogether studyTogether) {
         boolean rtn = studyTogetherService.updateById(studyTogether);
-		if(rtn) {
-			return success();
-		} else {
-			return error();
-		}
+        if (rtn) {
+            return success();
+        } else {
+            return error();
+        }
     }
 
-	
+    @ApiOperation(value = "学习记录统计", notes = "学习记录统计")
+    @SaCheckPermission("gen:studyTogether:statistics")
+    @GetMapping("/statisticsPage")
+    public String statisticsPage() {
+        return "admin/studyTogether/studyStatistics";
+    }
+
+
 }
